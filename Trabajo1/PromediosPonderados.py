121965cbc36f6c9722eac4f8cac971a381eb3a06
DavidbjArangoh <94635996+DavidbjArangoh@users.noreply.github.com>
2023-03-19 09:06:18 -0500

    creación del entorno de trabajo

--- Trabajo1/PromediosPonderados.py
+++ Trabajo1/PromediosPonderados.py
@@ -1,6 +1,13 @@
-try:
-    import gurobipy as gp
-    print('Gurobipy has been imported succesfully.')
-    
-except:
-    print('There is no gurobipy installed.')
+import gurobipy as gp
+
+# Definir grafo
+# Definir nodos (n)
+# nodos 1 y n son origen y destino
+# Definir arcos
+# Tener en cuenta qué:
+# Para los arcos no repetidos, la información es valida en cualquier dirección
+# Para los repetidos la información es valida solo en la dirección especificada
+# Objetivos:
+# 1. Minimizar la distancia total recorrida
+# 2. Minimizar las emisiones totales esperadas
+# 3. Minimizar el riesgo de exposición
